b572aaefe5d4d0c4fca35b35dc0c98cf36c86be0
madhes20 <118300068+madhes20@users.noreply.github.com>
2023-03-30 19:28:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,33 @@
 //your code here
+const issuesList = document.getElementById('issues-list');
+const pageHeading = document.getElementById('page-heading');
+let currentPageNumber = 1;
 
-
-let i = 0;
-
-fetch('https://api.github.com/repositories/1296269/issues?page=$%7BpageNumberHere%7D&per_page=5')
-.then((response) => response.json())
-.then((data) =>{
-    let myData = data;
-    console.log(data);
-    console.log(i)
-    console.log(myData[i].url);
-    let issue = document.getElementById('issue')
-    issue.innerHTML = `<a href ="${myData[i].url}">Issue ${i+1} </a>`
-    let pno = document.getElementById('pno');
-    pno.innerHTML = `<p>Page number ${i+1}</p>`
-})
-
-
-function next(){
-    
-    fetch('https://api.github.com/repositories/1296269/issues?page=$%7BpageNumberHere%7D&per_page=5')
-    .then((response) => response.json())
-    .then((data) =>{
-        if((i+1) < data.length)
-        {
-            i = i+1;
-            let myData = data;
-            console.log(i)
-            console.log(myData[i].url);
-            let issue = document.getElementById('issue')
-            issue.innerHTML = `<a href ="${myData[i].url}">Issue ${i+1} </a>` 
-
-            let pno = document.getElementById('pno');
-            pno.innerHTML = `<p>Page number ${i+1}</p>`
-        }
-        
-    })
+function getIssues() {
+  fetch(`https://api.github.com/repositories/1296269/issues?page=${currentPageNumber}&per_page=5`)
+    .then(response => response.json())
+    .then(issues => {
+      issuesList.innerHTML = '';
+      issues.forEach(issue => {
+        const issueItem = document.createElement('li');
+        issueItem.textContent = issue.title;
+        issuesList.appendChild(issueItem);
+      });
+    });
 }
 
+getIssues();
 
-function prev(){
-    
-    fetch('https://api.github.com/repositories/1296269/issues?page=$%7BpageNumberHere%7D&per_page=5')
-    .then((response) => response.json())
-    .then((data) =>{
-        if((i-1)>= 0)
-        {
-            i = i-1;
-            let myData = data;
-            console.log(i)
-            console.log(myData[i].url);
-            let issue = document.getElementById('issue')
-            issue.innerHTML = `<a href ="${myData[i].url}">Issue ${i+1} </a>` 
-            let pno = document.getElementById('pno');
-            pno.innerHTML = `<p>Page number ${i+1}</p>`
+document.getElementById('load_next').addEventListener('click', () => {
+  currentPageNumber++;
+  pageHeading.textContent = `Page number ${currentPageNumber}`;
+  getIssues();
+});
 
-        }
-        
-    })
-
-}
-
+document.getElementById('load_prev').addEventListener('click', () => {
+  if (currentPageNumber > 1) {
+    currentPageNumber--;
+    pageHeading.textContent = `Page number ${currentPageNumber}`;
+    getIssues();
+  }
+});
